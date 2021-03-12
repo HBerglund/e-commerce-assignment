3961084f15098c997a4c3a89add9d9d6159e8989
puppets101 <oscand92@gmail.com>
2021-03-12 15:35:07 +0100

    Added womens page rendering products

--- src/Assets/imageSources.ts
+++ src/Assets/imageSources.ts
@@ -2,6 +2,7 @@ const imageSources = {
   homePageHero: "https://images.unsplash.com/photo-1599901860904-17e6ed7083a0",
   productsPageHero:
     "https://images.unsplash.com/photo-1599447292325-2cffaa79bcbb",
+  womensPageHero: 'https://images.unsplash.com/photo-1518622358385-8ea7d0794bf6',
 };
 
 export default imageSources;
--- src/Navigation/Women.tsx
+++ src/Navigation/Women.tsx
@@ -1,8 +1,31 @@
-import React from "react";
-import { Typography } from "@material-ui/core";
+import ProductGrid from "../Components/ProductGrid";
+import json from "../products";
+import Product from "../productTypes";
+import Section from "../Components/Section";
+import Hero from "../Components/Hero";
+import imageSources from "../assets/imageSources";
 
-function Women() {
-  return <Typography variant="h1">Hello World</Typography>;
+let products: Product[] = json.Sheet1;
+let filteredProducts = products;
+
+function WomensProduct() {
+  filteredProducts = products.filter(
+    (product: Product) => product.category === "Womens clothing"
+  );
+
+  return (
+    <div>
+      <Hero
+        label='Bhagwan Yoga'
+        title='Womens products'
+        bgImg={imageSources.womensPageHero}
+        center
+      />
+      <Section>
+        <ProductGrid products={filteredProducts} />
+      </Section>
+    </div>
+  );
 }
 
-export default Women;
+export default WomensProduct;
