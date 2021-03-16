68dd8b48a908ffeeb4aa64a75ac80042a647b867
victorwikstrom <victor.wikstrom@icloud.com>
2021-03-16 16:26:30 +0100

    Fix responsivity in discover section and footer

--- src/Assets/imageSources.ts
+++ src/Assets/imageSources.ts
@@ -6,6 +6,10 @@ const imageSources = {
     "https://images.unsplash.com/photo-1518622358385-8ea7d0794bf6",
   mensPageHero: "https://images.unsplash.com/photo-1531403939386-c08a16cd7eef",
   faqPageHero: "https://images.unsplash.com/photo-1534551767192-78b8dd45b51b",
+  discover1:
+    "https://images.pexels.com/photos/2647471/pexels-photo-2647471.jpeg",
+  discover2: "https://images.unsplash.com/photo-1489659639091-8b687bc4386e",
+  discover3: "https://images.unsplash.com/photo-1594056113573-f8faae5ac78e",
 };
 
 export default imageSources;
