1ff3039e6dff51fedc302936aa986f81e84ac590
victorwikstrom <victor.wikstrom@icloud.com>
2021-03-22 15:47:32 +0100

    add login functionality. need to fix double click issue

--- src/Navigation/Router.tsx
+++ src/Navigation/Router.tsx
@@ -12,25 +12,26 @@ import NavBar from "../Components/NavBar";
 import ProductDetails from "./ProductDetails";
 import Contact from "./Contact";
 import Footer from "../Components/Footer";
-import Admin from "./Admin";
 import SignIn from "./SignIn";
+import Admin from "./Admin";
 
 function Router() {
   return (
     <BrowserRouter>
       <NavBar />
       <Switch>
-        <Route path='/' exact component={Home} />
-        <Route path='/products' exact component={Products} />
-        <Route path='/products/:id' component={ProductDetails} />
-        <Route path='/women' component={Women} />
-        <Route path='/men' component={Men} />
-        <Route path='/about' component={About} />
-        <Route path='/privacy' component={Privacy} />
-        <Route path='/faq' component={Faq} />
-        <Route path='/contact' component={Contact} />
-        <Route path='/checkout' component={Checkout} />
-        <Route path='/admin' component={SignIn} />
+        <Route path="/" exact component={Home} />
+        <Route path="/products" exact component={Products} />
+        <Route path="/products/:id" component={ProductDetails} />
+        <Route path="/women" component={Women} />
+        <Route path="/men" component={Men} />
+        <Route path="/about" component={About} />
+        <Route path="/privacy" component={Privacy} />
+        <Route path="/faq" component={Faq} />
+        <Route path="/contact" component={Contact} />
+        <Route path="/checkout" component={Checkout} />
+        <Route path="/admin" component={Admin} />
+        <Route path="/signin" component={SignIn} />
       </Switch>
       <Footer />
     </BrowserRouter>
