39dff4179155131c4071b1fa471c1484185e0cc9
puppets101 <oscand92@gmail.com>
2021-03-23 13:38:49 +0100

    Working remove product item

--- src/Components/AdminListItem.tsx
+++ src/Components/AdminListItem.tsx
@@ -1,10 +1,14 @@
 import {
   createStyles,
   Divider,
+  IconButton,
   makeStyles,
   Typography,
 } from "@material-ui/core";
-import { Product } from "../Context/ProductListContext";
+import { Product, ProductsContext } from "../Context/ProductListContext";
+import EditIcon from "@material-ui/icons/Edit";
+import DeleteIcon from "@material-ui/icons/Delete";
+import { useContext } from "react";
 
 interface Props {
   product: Product;
@@ -34,24 +38,38 @@ function AdminListItem(props: Props) {
   );
   const classes = useStyles();
 
+  const productsContext = useContext(ProductsContext);
+  const products: Product[] = productsContext.list;
+
   const { product } = props;
   return (
     <div className={classes.root}>
       <div>
-        <Typography variant="h6">{product.name}</Typography>
-        <Typography variant="body1" gutterBottom>
+        <Typography variant='h6'>
+          {product.name}
+          <IconButton
+            onClick={() => productsContext.deleteProduct(product)}
+            size='small'
+          >
+            <DeleteIcon fontSize='small' />
+          </IconButton>
+        </Typography>
+        <Typography variant='body1' gutterBottom>
           {product.description}
+          <IconButton size='small'>
+            <EditIcon fontSize='small' />
+          </IconButton>
         </Typography>
       </div>
       <div className={classes.mapWrapper}>
-        <Typography variant="body2" gutterBottom>
+        <Typography variant='body2' gutterBottom>
           Colors
         </Typography>
         <div className={classes.propsWrapper}>
           {product.colorProps.map(({ color, img }) => (
             <div>
               <img src={img} alt={product.name} className={classes.image} />
-              <Typography variant="body1" gutterBottom>
+              <Typography variant='body1' gutterBottom>
                 {color}
               </Typography>
             </div>
@@ -59,16 +77,16 @@ function AdminListItem(props: Props) {
         </div>
       </div>
       <div className={classes.mapWrapper}>
-        <Typography variant="body2" gutterBottom>
+        <Typography variant='body2' gutterBottom>
           Size and price
         </Typography>
         <div className={classes.propsWrapper}>
           {product.sizeProps.map(({ size, price }) => (
             <div>
-              <Typography variant="body1" gutterBottom>
+              <Typography variant='body1' gutterBottom>
                 {size}
               </Typography>
-              <Typography variant="body1" gutterBottom>
+              <Typography variant='body1' gutterBottom>
                 ${price}
               </Typography>
             </div>
--- src/Context/ProductListContext.tsx
+++ src/Context/ProductListContext.tsx
@@ -1,3 +1,4 @@
+import { cloneDeep, isEqual } from "lodash";
 import { createContext, FC, useState, useEffect } from "react";
 import json from "../products";
 
@@ -13,23 +14,44 @@ export interface Product {
 
 interface ProductsValue {
   list: Product[];
+  deleteProduct: (product: Product) => void;
 }
 
 export const ProductsContext = createContext<ProductsValue>({
   list: [],
+  deleteProduct: () => {},
 });
 
+const getDefaultProducts = () => {
+  const productsInLs = localStorage.getItem("productList");
+  if (productsInLs) {
+    return JSON.parse(productsInLs);
+  } else {
+    return json.Sheet1;
+  }
+};
+
 const ProductsProvider: FC<{}> = ({ children }) => {
-  const [list, setList] = useState<Product[]>(json.Sheet1);
+  const [list, setList] = useState<Product[]>(getDefaultProducts());
 
   useEffect(() => {
     localStorage.setItem("productList", JSON.stringify(list));
   }, [list]);
 
+  const deleteProduct = (product: Product) => {
+    const clonedList = cloneDeep(list);
+    const foundIndex = clonedList.findIndex((p) => isEqual(p, product));
+    const updatedList = clonedList.filter(
+      (p) => clonedList.indexOf(p) !== foundIndex
+    );
+    setList(updatedList);
+  };
+
   return (
     <ProductsContext.Provider
       value={{
         list,
+        deleteProduct,
       }}
     >
       {children}
