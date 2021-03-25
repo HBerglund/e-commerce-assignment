484014148d71f100a47fe97eba0b442494c29de1
victorwikstrom <victor.wikstrom@icloud.com>
2021-03-25 14:50:11 +0100

    Add reset functionality and some eye pleasing animations

--- src/Navigation/Admin.tsx
+++ src/Navigation/Admin.tsx
@@ -4,6 +4,7 @@ import { ProductsContext, Product } from "../Context/ProductListContext";
 import AdminListItem from "../Components/AdminListItem";
 import {
   Button,
+  Collapse,
   Divider,
   FormControl,
   InputLabel,
@@ -18,6 +19,7 @@ function Admin() {
   const products: Product[] = productsContext.list;
 
   const [showForm, setShowForm] = useState<boolean>(false);
+  const [showCategories, setShowCategories] = useState<boolean>(false);
   const [category, setCategory] = useState<string>(products[0].category);
 
   let newProduct: Product = {
@@ -29,6 +31,12 @@ function Admin() {
     sizeProps: [{ size: "", price: "" }],
     description: "",
   };
+
+  let catArr: string[] = [];
+  for (const product of productsContext.list) {
+    catArr.push(product.category);
+  }
+  const categories: string[] = [...new Set(catArr)];
 
   const handleNewProductInputs = (e: ChangeEvent<HTMLInputElement>) => {
     const value = e.target.value;
@@ -66,12 +74,8 @@ function Admin() {
     };
   };
 
-  const handleClose = () => {
-    setShowForm(false);
-  };
-
-  const handleOpen = () => {
-    setShowForm(true);
+  const handleShowCategories = () => {
+    setShowCategories((prev) => !prev);
   };
 
   const handleCategoryInput = (e: React.ChangeEvent<{ value: unknown }>) => {
@@ -85,92 +89,98 @@ function Admin() {
     setShowForm(false);
   };
 
+  const handleShowForm = () => {
+    setShowForm((prev) => !prev);
+  };
+
   return (
     <Section>
       <div style={{ marginBottom: "1rem", width: "100%" }}>
         <Typography
           style={{ cursor: "pointer", marginBottom: "1rem" }}
           variant="body1"
-          onClick={() => {
-            setShowForm((prevState) => !prevState);
-          }}
+          onClick={handleShowForm}
         >
           Add new product
         </Typography>
-        <div
-          style={{
-            display: "flex",
-            flexDirection: "column",
-            alignItems: "start",
-            minWidth: "600px",
-          }}
-        >
-          <FormControl>
-            <InputLabel>Category</InputLabel>
-            <Select
-              name="category"
-              value={category}
-              open={showForm}
-              onOpen={handleOpen}
-              onClose={handleClose}
-              onChange={handleCategoryInput}
+        <Collapse in={showForm}>
+          <form onSubmit={handleAddNewProductClick}>
+            <div
+              style={{
+                display: "flex",
+                flexDirection: "column",
+                alignItems: "start",
+                minWidth: "600px",
+              }}
             >
-              {products.map(({ category }) => (
-                <MenuItem value={category}>{category}</MenuItem>
-              ))}
-            </Select>
-          </FormControl>
-          <TextField
-            name="name"
-            onChange={handleNewProductInputs}
-            style={{ minWidth: "600px" }}
-            label="Product name"
-          />
-          <TextField
-            name="fabric"
-            onChange={handleNewProductInputs}
-            style={{ minWidth: "600px" }}
-            label="Fabric"
-          />
-          <TextField
-            name="color"
-            onChange={handleNewColorInputs}
-            style={{ minWidth: "600px" }}
-            label="Color"
-          />
-          <TextField
-            name="img"
-            onChange={handleNewColorInputs}
-            style={{ minWidth: "600px" }}
-            label="Image source"
-          />
-          <TextField
-            name="size"
-            onChange={handleNewSizeInputs}
-            style={{ minWidth: "600px" }}
-            label="Size"
-          />
-          <TextField
-            name="price"
-            onChange={handleNewSizeInputs}
-            style={{ minWidth: "600px" }}
-            label="Price"
-          />
-          <TextField
-            name="description"
-            onChange={handleNewProductInputs}
-            style={{ minWidth: "600px" }}
-            multiline
-            label="Description"
-          />
-          <Button
-            onClick={handleAddNewProductClick}
-            variant={"contained"}
-            style={{ margin: "1rem 0" }}
-          >
-            Save
-          </Button>
-        </div>
+              <FormControl>
+                <InputLabel onClick={handleShowCategories}>Category</InputLabel>
+                <Select
+                  name="category"
+                  value={category}
+                  open={showCategories}
+                  onOpen={handleShowCategories}
+                  onClose={handleShowCategories}
+                  onChange={handleCategoryInput}
+                >
+                  {categories.map((category) => (
+                    <MenuItem value={category}>{category}</MenuItem>
+                  ))}
+                </Select>
+              </FormControl>
+              <TextField
+                name="name"
+                onChange={handleNewProductInputs}
+                style={{ minWidth: "600px" }}
+                label="Product name"
+              />
+              <TextField
+                name="fabric"
+                onChange={handleNewProductInputs}
+                style={{ minWidth: "600px" }}
+                label="Fabric"
+              />
+              <TextField
+                name="color"
+                onChange={handleNewColorInputs}
+                style={{ minWidth: "600px" }}
+                label="Color"
+              />
+              <TextField
+                name="img"
+                onChange={handleNewColorInputs}
+                style={{ minWidth: "600px" }}
+                label="Image source"
+              />
+              <TextField
+                name="size"
+                onChange={handleNewSizeInputs}
+                style={{ minWidth: "600px" }}
+                label="Size"
+              />
+              <TextField
+                name="price"
+                onChange={handleNewSizeInputs}
+                style={{ minWidth: "600px" }}
+                label="Price"
+              />
+              <TextField
+                name="description"
+                onChange={handleNewProductInputs}
+                style={{ minWidth: "600px" }}
+                multiline
+                label="Description"
+              />
+              <Button
+                type="submit"
+                variant={"contained"}
+                style={{ margin: "1rem 0" }}
+              >
+                Save
+              </Button>
+            </div>
+          </form>
+        </Collapse>
       </div>
       <Divider style={{ marginBottom: "1rem" }} />
       {products.map((product: Product) => (
